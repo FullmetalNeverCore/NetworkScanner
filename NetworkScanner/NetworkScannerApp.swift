13e5946be64a856bdc869a55a3cb1c2e1bc8e7e9
0xNeverC0RE <godsyndrome9@gmail.com>
2024-02-22 22:47:11 +0200

    Initial Commit

---
+++ NetworkScanner/NetworkScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkScannerApp.swift
+//  NetworkScanner
+//
+//  Created by 0xNeverC0RE on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
